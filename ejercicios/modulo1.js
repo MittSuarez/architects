856e7b303b776228c4b8ebb65a60cf7bc1c1dc7c
MittSuarez <mittsuarez@gmail.com>
2024-01-25 20:03:18 -0500

    push de ejercicio 1 de javascript

--- ejercicios/modulo1.js
+++ ejercicios/modulo1.js
@@ -1,9 +1,24 @@
 
-
-
-
 // Ejemplo
 let litro = 5;
 const litrosToCentilitros = litro * 100;
 
 console.log('Texto cualquiera', litrosToCentilitros);
+
+
+// EJERCICIOS EN JAVASCRIPT
+
+// 1. Realizar el algoritmo que convierta litros en centilitros, decilitros, galones y onzas
+
+let litros = 1;
+
+const centilitros = litros * 100;
+const decilitros = litros * 10;
+const galones = litros * 0.264172;
+const onzas = litros * 33.814;
+
+console.log("Un litro a Centilitros es: ", centilitros);
+console.log("Un litro a Decilitros es: ", decilitros);
+console.log("Un litro a Galones es: ", galones);
+console.log("Un litro a Onzas es: ", onzas);
+

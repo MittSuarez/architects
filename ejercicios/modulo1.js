6c4d92998f7523af7248dcc251ead1b0bf0ded78
MittSuarez <mittsuarez@gmail.com>
2024-01-25 20:11:40 -0500

    push de ejercicio 2 de javascript

--- ejercicios/modulo1.js
+++ ejercicios/modulo1.js
@@ -22,3 +22,21 @@ console.log("Un litro a Decilitros es: ", decilitros);
 console.log("Un litro a Galones es: ", galones);
 console.log("Un litro a Onzas es: ", onzas);
 
+
+// 2. Realizar el algoritmo que realice las 4 operaciones básicas para dos números de entrada
+
+let num1 = 5;
+let num2 = 2;
+
+const suma = num1 + num2;
+const resta = num1 - num2;
+const multiplicacion = num1 * num2;
+const division = num1 / num2;
+
+console.log("Suma: ", suma);
+console.log("Resta: ", resta);
+console.log("Multiplicación: ", multiplicacion);
+console.log("División: ", division);
+
+
+

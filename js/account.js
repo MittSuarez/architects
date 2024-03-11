a3daec9d96628d322b17be16a92f5f08bdbd550d
MittSuarez <mittsuarez@gmail.com>
2024-03-10 22:07:58 -0500

    actualizacion de entrega modulo JavaScript

--- js/account.js
+++ js/account.js
@@ -1,47 +1,48 @@
 import * as users from './users.js';
 
+function mostrarDatosUsers(listUsers, emailBuscado){
+    
+    for (let i = 0; i < listUsers.length; i++) {
+        if (listUsers[i].email === emailBuscado) {
+            console.log(listUsers[i].nombre);
+            
+            let lista = "<h2>INFORMACION DE USUARIO</h2>";
+            lista += `<div class="listaContenedor"> 
+                    <h2 class="titulo">${listUsers[i].nombre}</h2>
+                    <span>${listUsers[i].email}</span>
+                    <p>${listUsers[i].descripcion}</p>
+                </div>`
+            document.getElementById('informacionDeUsuarios').innerHTML = lista
+        }
+    }
 
-
-
-const listUsers = users.users;
-
-
-const emailBuscado = localStorage.getItem('email');
-
-
-// for (let i = 0; i < usuarios.length; i++) {
-
-//     if (usuarios[i].email === emailBuscado) {
-//         console.log('El correo electrónico ' + emailBuscado + ' pertenece a ' + usuarios[i].nombre);
-//         // const name = usuarios[i].nombre;
-//         // const email = usuarios[i].email;
-//         // const description = usuarios[i].descripcion;
-//     }
-// }
-
-
-
-
-function mostrarDatosUsers(listUsers){
-    let lista = "<h2>Listado de servicios aqui</h2>";
-    listUsers.forEach(element => {
-        lista += `<div class="listaContenedor"> 
-            <h1 class="titulo">${element.nombre}</h1>
-            <span>${element.email}</span>
-            <p>${element.descripcion}</p>
-        </div>`
-    });
-
-    document.getElementById('informacionDeUsuarios').innerHTML = lista
 }
 
-mostrarDatosUsers(listUsers) 
+const listUsers = users.users;
+const emailBuscado = localStorage.getItem('email');
 
+mostrarDatosUsers(listUsers, emailBuscado) 
 
+// APIs
+const informacionApiPoke = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(respuesta => respuesta.json())
+    .catch(error => console.log('Error: ', error))
 
+informacionApiPoke.then((respuestaConDatos) => {
+    //console.log(respuestaConDatos.abilities)
+    const elemento = document.getElementById('informacionDeApi');
+    
+    const nombre = document.createElement('h2')
+    nombre.textContent = respuestaConDatos.name
+    elemento.appendChild(nombre)
 
+    respuestaConDatos.abilities.forEach(element => {
+        const parrafo = document.createElement('p')
+        parrafo.textContent = element.ability.url
+        elemento.appendChild(parrafo)
+    });
 
-
+})
 
 
 
@@ -49,3 +50,6 @@ mostrarDatosUsers(listUsers)
 console.log(users.users);
 const divtest = localStorage.getItem('email')
 console.log(divtest)
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log('ingreso a la geolocalizacion')
+})
